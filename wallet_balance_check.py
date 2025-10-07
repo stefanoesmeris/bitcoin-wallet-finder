3b45d03779586fb26d3278014cf755b5f8505bc1
stefanoesmeris <62100873+stefanoesmeris@users.noreply.github.com>
2025-10-07 14:38:50 -0300

    Add files via upload

--- wallet_balance_check.py
+++ wallet_balance_check.py
@@ -1,11 +1,13 @@
 import json
 import requests
+
 from bip_utils import (
     Bip39SeedGenerator, Bip39MnemonicValidator,
     Bip44, Bip49, Bip84,
     Bip44Coins, Bip49Coins, Bip84Coins,
-    Bip44Changes
+    Bip44Changes, Bip44Levels, Bip44DepthError, Bip32Slip10Secp256k1, P2PKHAddr
 )
+
 
 # === Funções auxiliares ===
 def derivar_enderecos(mnemonic, tipo):
@@ -25,6 +27,43 @@ def derivar_enderecos(mnemonic, tipo):
 
     carteira = bip.Purpose().Coin().Account(0).Change(Bip44Changes.CHAIN_EXT)
     return [carteira.AddressIndex(i).PublicKey().ToAddress() for i in range(20)]
+
+
+#from bip_utils import Bip39SeedGenerator, Bip39MnemonicValidator, Bip32Slip10Secp256k1, P2pkhAddr
+
+def derivar_enderecos_liquid(mnemonic):
+    #if not Bip39MnemonicValidator(mnemonic).IsValid():
+    #    return []
+
+    seed_bytes = Bip39SeedGenerator(mnemonic).Generate()
+    bip32_ctx = Bip32Slip10Secp256k1.FromSeed(seed_bytes)
+
+    enderecos = []
+    for i in range(20):
+        path = f"m/44'/1776'/0'/0/{i}"
+        chave = bip32_ctx.DerivePath(path)
+        endereco = P2PKHAddr.EncodeKey(chave.PublicKey())
+        enderecos.append(endereco)
+
+    return enderecos
+
+
+#def derivar_enderecos_liquid(mnemonic):
+    #if not Bip39MnemonicValidator(mnemonic).IsValid():
+    #    return []
+
+#    seed_bytes = Bip39SeedGenerator(mnemonic).Generate()
+    # Derivação manual: m/44'/1776'/0'/0/i
+#    base_path = "m/44'/1776'/0'/0"
+#    bip = Bip44.FromSeedAndPath(seed_bytes, base_path)
+#
+#    try:
+#        carteira = bip.Account(0).Change(Bip44Changes.CHAIN_EXT)
+#        return [carteira.AddressIndex(i).PublicKey().ToAddress() for i in range(20)]
+#    except Bip44DepthError:
+#        return []
+
+
 
 def saldo_blockstream(endereco, rede="btc"):
     if rede == "btc":
@@ -57,10 +96,18 @@ for grupo in dados:
 
         enderecos = derivar_enderecos(mnemonic, tipo)
         saldo_total_btc = sum(saldo_blockstream(addr, "btc") for addr in enderecos)
-        saldo_total_liq = sum(saldo_blockstream(addr, "liquid") for addr in enderecos)
+        #enderecos = derivar_enderecos_liquid(mnemonic)
+        #saldo_total_liq = sum(saldo_blockstream(addr, "liquid") for addr in enderecos)
 
         print(f"\n🔐 Tipo: {tipo}")
-        print(f"📜 Mnemonic: {mnemonic[:-30]}...")  # mostra só o início por segurança
+        #print(f"📜 Mnemonic: {mnemonic[:-30]}...")  # mostra só o início por segurança
         print(f"📜 Últimas palavras: {' '.join(mnemonic.split()[-3:])}")
-        print(f"   ➤ Saldo total on-chain: {saldo_total_btc:.8f} BTC")
-        print(f"   ➤ Saldo total Liquid:   {saldo_total_liq:.8f} L-BTC")
+        if saldo_total_btc > 0:
+            print(f"  ✅ Saldo total on-chain: {saldo_total_btc:.8f} BTC")
+        else:
+            print(f"   ➤ Saldo total on-chain: {saldo_total_btc:.8f} BTC")
+        #if saldo_total_liq > 0:
+        #    print(f"  ✅ Saldo total Liquid:   {saldo_total_liq:.8f} L-BTC")
+        #else:          
+        #    print(f"   ➤ Saldo total Liquid:   {saldo_total_liq:.8f} L-BTC")
+#
