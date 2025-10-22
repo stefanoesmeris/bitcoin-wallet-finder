ad3ee2b6c442c812ce7ffd1f75365d69e9a56eed
stefanoesmeris <62100873+stefanoesmeris@users.noreply.github.com>
2025-10-22 16:16:47 -0300

    Add files via upload

--- general_functions.py
+++ general_functions.py
@@ -1,8 +1,7 @@
-
 from mnemonic import Mnemonic
 import bip_utils
 import requests
-import time, os, json, argparse
+import time, os, json
 from datetime import datetime
 
 from bip_utils import (
@@ -17,42 +16,27 @@ class General_Functions:
         self.N = 12
         self.M = 0
         self.U = "http://127.0.0.1:5000/api"
-        
-    def get_next(self, M, N):
-        # Inicializa o gerador BIP39 em inglês
+        self.SETUP_FILE = 'setup.json'
+        self.DEFAULT_N = 0
+        self.DEFAULT_U = "http://127.0.0.1:8080/api"
+        self.DEFAULT_CONTADOR = 0
+
+    @staticmethod
+    def get_next(M, N):
         mnemo = Mnemonic("english")
-
-        # Define o número de palavras da seed (12, 15, 18, 21 ou 24)
-        #N = 12
-        #M = 0 # Index of word from BIP39
-
-        # Pega a primeira palavra da lista BIP39
-        first_word = mnemo.wordlist[M]  # geralmente 'abandon'
-
-        # Cria a base da frase com N - 1 repetições da primeira palavra
+        first_word = mnemo.wordlist[M]
         base_phrase = [first_word] * (N - 1)
-
-        # Dicionário para armazenar frases válidas
-        #valid_phrases = {}
         my_list = []
 
-        # Testa todas as palavras possíveis como última palavra
         for candidate in mnemo.wordlist:
             test_phrase = base_phrase + [candidate]
             phrase_str = " ".join(test_phrase)
             if mnemo.check(phrase_str):
-                #valid_phrases[candidate] = phrase_str
                 my_list.append(phrase_str)
 
-        # Exibe os resultados
-        #print(f"Frases válidas encontradas: {len(valid_phrases)}")
-        #for word, phrase in valid_phrases.items():
-        #    print(f"{phrase}")
+        return my_list
 
-        return(my_list)
-    #
     def consultar_wallets(self):
-        # Carrega da API uma lista de wallets descobertas.
         try:
             resposta = requests.get(self.U)
             if resposta.status_code == 200:
@@ -61,38 +45,29 @@ class General_Functions:
                 print("Erro ao consultar:", resposta.status_code)
                 return []
         except requests.exceptions.RequestException as e:
-            print(f"[Tentativa {attempt+1}] Erro de conexão: {e}")
-            time.sleep(2 ** attempt)  # Backoff exponencial
-        # Se todas as tentativas falharem
+            print(f"Erro de conexão: {e}")
+            time.sleep(2)
         print("Falha ao consultar a API após múltiplas tentativas.")
         return False
 
     def manipular_configuracao(self, acao, novos_valores=None):
         if acao == 'ler':
-            if os.path.exists(SETUP_FILE):
-                with open(SETUP_FILE, 'r') as f:
+            if os.path.exists(self.SETUP_FILE):
+                with open(self.SETUP_FILE, 'r') as f:
                     return json.load(f)
             else:
                 return None
         elif acao == 'atualizar' and novos_valores:
-            with open(SETUP_FILE, 'w') as f:
+            with open(self.SETUP_FILE, 'w') as f:
                 json.dump(novos_valores, f, indent=4)
-        
-    def enviar_wallets(self, dados):
-        """
-        Envia os dados diretamente como lista de dicionários para a API Flask via POST.
-        Se necessário, encapsula em uma lista de listas para compatibilidade com a API.
 
-        :param dados: Lista de dicionários com os dados das wallets.
-        :param url_api: URL da API Flask.
-        """
+    def enviar_wallets(self, dados):
         try:
-            # Garante que os dados estejam no formato [[{...}, {...}]]
             if isinstance(dados, list) and all(isinstance(item, dict) for item in dados):
-                dados_formatados = [dados]  # encapsula em uma lista externa
+                dados_formatados = [dados]
             else:
                 raise ValueError("Formato inválido: esperado lista de dicionários.")
-            
+
             resposta = requests.post(self.U, json=dados_formatados)
 
             if resposta.status_code == 200:
@@ -104,8 +79,7 @@ class General_Functions:
 
         except Exception as e:
             print("❌ Erro ao processar:", e)
-    # 
-    # Consulta histórico de transações via Blockstream API
+
     def has_activity(self, addr, max_retries=5, timeout=5):
         url = f"https://blockstream.info/api/address/{addr}/txs"
         for attempt in range(max_retries):
@@ -113,24 +87,18 @@ class General_Functions:
                 response = requests.get(url, timeout=timeout)
                 if response.status_code == 200:
                     txs = response.json()
-                    # Verifica se é uma lista e se contém dados
-                    if isinstance(txs, list) and txs:
-                        return True
-                    else:
-                        # Dados vazios ou não listados
-                        return False
+                    return isinstance(txs, list) and bool(txs)
                 elif response.status_code == 429:
-                    print(f"[Tentativa {attempt+1}] - Codigo 429  servidor recusando - aguardar {(attempt+1) *5} minutos {datetime.now()}")
-                    time.sleep(300  * (attempt + 1))
+                    print(f"[{attempt+1}] Código 429 - aguardando {(attempt+1) * 5} minutos")
+                    time.sleep(300 * (attempt + 1))
                 else:
-                    print(f"[Tentativa {attempt+1}] Código de status inesperado: {response.status_code}")
+                    print(f"[{attempt+1}] Código inesperado: {response.status_code}")
             except requests.exceptions.RequestException as e:
-                print(f"[Tentativa {attempt+1}] Erro de conexão: {e}")
-                time.sleep(2 ** attempt)  # Backoff exponencial
-        # Se todas as tentativas falharem
+                print(f"[{attempt+1}] Erro de conexão: {e}")
+                time.sleep(2 ** attempt)
         print("Falha ao consultar a API após múltiplas tentativas.")
         return False
-    #
+
     def saldo_blockstream(self, addr, rede="btc", max_retries=5, timeout=5):
         if rede == "btc":
             url = f"https://blockstream.info/api/address/{addr}"
@@ -138,6 +106,7 @@ class General_Functions:
             url = f"https://blockstream.info/liquid/api/address/{addr}"
         else:
             return 0
+
         for attempt in range(max_retries):
             try:
                 r = requests.get(url, timeout=timeout)
@@ -147,15 +116,15 @@ class General_Functions:
                     spent = dados.get("chain_stats", {}).get("spent_txo_sum", 0)
                     return (funded - spent) / 1e8
                 else:
-                    print(f"[Tentativa {attempt+1}] Código de status inesperado: {r.status_code}") 
-                    time.sleep(60  * (attempt + 1))
+                    print(f"[{attempt+1}] Código inesperado: {r.status_code}")
+                    time.sleep(60 * (attempt + 1))
             except requests.exceptions.RequestException as e:
-                print(f"[Tentativa {attempt+1}] Erro de conexão: {e}")
-                time.sleep(2 ** attempt)  # Backoff exponencial
-                #pass
+                print(f"[{attempt+1}] Erro de conexão: {e}")
+                time.sleep(2 ** attempt)
         return 0
-    #
-    def write_good_seed_to_file(self, data, filename):
+
+    @staticmethod
+    def write_good_seed_to_file(data, filename):
         if os.path.exists(filename):
             with open(filename, "r") as f:
                 try:
@@ -164,24 +133,22 @@ class General_Functions:
                     dados_existentes = []
         else:
             dados_existentes = []
-        # Adiciona o novo dado à lista
+
         dados_existentes.append(data)
-        # Salva de volta no arquivo
         with open(filename, "w") as f:
             json.dump(dados_existentes, f, indent=4)
-        #print("\n✅ Resultado exportado para ", str(filename))
-    #
-    # Função para derivar e verificar endereços
+
     def check_addresses(self, derivation, coin_type, label, bip_number, mnemonic):
-        D = 3 # Deriva os x primeiros endereços da carteira.
+        D = 3
         seed_bytes = Bip39SeedGenerator(mnemonic).Generate()
         wallet = derivation.FromSeed(seed_bytes, coin_type)
         results = []
         good_seed = False
-        for i in range(D):  # Varrer os primeiros D endereços externos
+
+        for i in range(D):
             path = f"m/{bip_number}'/0'/0'/0/{i}"
             addr = wallet.Purpose().Coin().Account(0).Change(Bip44Changes.CHAIN_EXT).AddressIndex(i).PublicKey().ToAddress()
-            active = self.has_activity(self, addr)
+            active = self.has_activity(addr)
             status = "Movimentado" if active else "unused"
             if active:
                 good_seed = True
@@ -190,16 +157,13 @@ class General_Functions:
                     "Address": addr,
                     "Path": path,
                     "Status": status,
-                    "Mnemonic": mnemonic  # ⚠️ Cuidado: incluir apenas se necessário
+                    "Mnemonic": mnemonic
                 })
                 print("Found a good seed")
-                break # Basta encontrar um unico endereço que ja e suficiente!  Just finding a single address is enough!
-        if good_seed:    
-            self.write_good_seed_to_file(self, results, "dados.json")
-            self.enviar_wallets(self, results) # grava os dados numa API
-            #print(results)
+                break
+
+        if good_seed:
+            self.write_good_seed_to_file(results, "dados.json")
+            self.enviar_wallets(results)
+
         return results
-
-
-
-
--- wallet_find_transactions.py
+++ wallet_find_transactions.py
@@ -1,22 +1,9 @@
 # git clone https://github.com/stefanoesmeris/bitcoin-wallet-finder.git
 # pip install bip-utils requests Mnemonic
-#
-# O que este script faz:
-# 
-# 	Importa as palavras sementes
-# 	Deriva os endereços nos padrões BIP44, BIP49 e BIP84
-# 	Verifica se houve movimentações (mesmo com saldo zerado)
-# 	Exporta os resultados para um arquivo  contendo:
-# 	Endereço
-# 	Caminho de derivação (ex: )
-# 	Tipo (BIP44, BIP49, BIP84)
-# 	Status de movimentação
-# 	Palavras sementes (opcional — cuidado com segurança!)
-#import bip_utils
+
 import requests
 import time, os, json, argparse
 from general_functions import General_Functions as GF
-
 
 from bip_utils import (
     Bip39SeedGenerator, Bip39MnemonicValidator, Bip39MnemonicGenerator,
@@ -25,133 +12,106 @@ from bip_utils import (
     Bip44Changes, Bip39WordsNum
 )
 
-SETUP_FILE = 'setup.json'
-DEFAULT_N = 0 # N = 12 - Define o número de palavras da seed (12, 15, 18, 21 ou 24) ou 0(zero) para aleatorio
-DEFAULT_U = "http://127.0.0.1:8080/api"
-DEFAULT_CONTADOR = 0
 
-
-def manipular_configuracao(acao, novos_valores=None):
-    if acao == 'ler':
-        if os.path.exists(SETUP_FILE):
-            with open(SETUP_FILE, 'r') as f:
-                return json.load(f)
-        else:
-            return None
-    elif acao == 'atualizar' and novos_valores:
-        with open(SETUP_FILE, 'w') as f:
-            json.dump(novos_valores, f, indent=4)
+# Instância global da classe
+gf = GF()
 
 def get_mnemonic_seq(M, N):
-    #N = 12 # Define o número de palavras da seed (12, 15, 18, 21 ou 24)
-    #M = 0  # Index of word from BIP39
-    #seq = GF(0)
-    lista = []
-    lista = GF.get_next(GF, int(M),int(N)) # Busca uma lista de mnemonic para ser avaliada.
+    lista = gf.get_next(int(M), int(N))
     X = 0
     for phrase in lista:
         mnemonic = str(phrase)
-        data = []
-        inicio = time.time()
-        print("CheckSum ", X, "of ", len(lista)-1, "Seed ", mnemonic)
-        data = GF.check_addresses(GF, Bip44, Bip44Coins.BITCOIN, "Legacy (BIP44)", 44, mnemonic)
+        print("CheckSum", X, "of", len(lista)-1, "Seed", mnemonic)
+
+        gf.check_addresses(Bip44, Bip44Coins.BITCOIN, "Legacy (BIP44)", 44, mnemonic)
         print("Looking BIP44 Legacy\n")
-        time.sleep(0.5)  # Sleep for half a second
-        data = GF.check_addresses(GF, Bip49, Bip49Coins.BITCOIN, "P2SH (BIP49)", 49, mnemonic)
+        time.sleep(0.5)
+
+        gf.check_addresses(Bip49, Bip49Coins.BITCOIN, "P2SH (BIP49)", 49, mnemonic)
         print("Looking BIP49 P2SH\n")
-        time.sleep(0.5)  # Sleep for half a second
-        data = GF.check_addresses(GF, Bip84, Bip84Coins.BITCOIN, "SegWit (BIP84)", 84, mnemonic)
+        time.sleep(0.5)
+
+        gf.check_addresses(Bip84, Bip84Coins.BITCOIN, "SegWit (BIP84)", 84, mnemonic)
         print("Looking BIP84 SegWit\n")
-        time.sleep(0.5)  # Sleep for half a second
+        time.sleep(0.5)
+
         fim = time.time()
-        tempo_total = fim - inicio
+        tempo_total = fim - time.time()
         print(f"A tarefa levou {tempo_total:.4f} segundos para ser concluída.\n")
         X += 1
-        #if X % 15 == 0:
-        #    print("sleep for 15 seconds - because no one is made of iron")
-        #    time.sleep(15)  # Sleep for 15 seconds
-#
+
 def get_mnemonic_rand():
     mnemonic = Bip39MnemonicGenerator().FromWordsNumber(Bip39WordsNum.WORDS_NUM_12)
     print(f"Mnemonic string: {mnemonic}")
-    # Gera a seed
-    #seed_bytes = Bip39SeedGenerator(mnemonic).Generate()
-    # Executa para os três padrões
-    data = []
+
     inicio = time.time()
-    data = GF.check_addresses(GF, Bip44, Bip44Coins.BITCOIN, "Legacy (BIP44)", 44, mnemonic)
+
+    gf.check_addresses(Bip44, Bip44Coins.BITCOIN, "Legacy (BIP44)", 44, mnemonic)
     print("Looking BIP44 Legacy\n")
-    time.sleep(0.5)  # Sleep for half a second
-    data = GF.check_addresses(GF, Bip49, Bip49Coins.BITCOIN, "P2SH (BIP49)", 49, mnemonic)
+    time.sleep(0.5)
+
+    gf.check_addresses(Bip49, Bip49Coins.BITCOIN, "P2SH (BIP49)", 49, mnemonic)
     print("Looking BIP49 P2SH\n")
-    time.sleep(0.5)  # Sleep for half a second
-    data = GF.check_addresses(GF, Bip84, Bip84Coins.BITCOIN, "SegWit (BIP84)", 84, mnemonic)
+    time.sleep(0.5)
+
+    gf.check_addresses(Bip84, Bip84Coins.BITCOIN, "SegWit (BIP84)", 84, mnemonic)
     print("Looking BIP84 SegWit\n")
-    time.sleep(0.5)  # Sleep for half a second
+    time.sleep(0.5)
+
     fim = time.time()
     tempo_total = fim - inicio
     print(f"A tarefa levou {tempo_total:.4f} segundos para ser concluída.\n")
-    
-#    
+
 def main():
     print("🔁 Starting main loop. Press Ctrl+C to stop.")
-    # N = 12 # Define o número de palavras da seed (12, 15, 18, 21 ou 24)
-    #
+
     parser = argparse.ArgumentParser(description="Script com configuração persistente")
     parser.add_argument('--N', type=int, help="Valor inicial para N (prioridade máxima)")
     parser.add_argument('--U', type=str, help="Valor inicial para URL (prioridade máxima)")
     args = parser.parse_args()
-    config = manipular_configuracao('ler')
-    #
-    # 🧠 Determina valor de N com base na prioridade
+
+    config = gf.manipular_configuracao('ler')
+
     if args.N is not None:
         N = args.N
     elif config and 'N' in config:
         N = config['N']
     else:
-        N = DEFAULT_N
+        N = gf.DEFAULT_N
 
-    # 🧠 Determina valor de U com base na prioridade
     if args.U is not None:
         U = args.U
     elif config and 'U' in config:
         U = config['U']
     else:
-        U = DEFAULT_U
-        
-    # 🧱 Inicializa contador
-    contador = config['contador'] if config and 'contador' in config else DEFAULT_CONTADOR
-    
-    GF.U = U 
-    
-    # 💾 Atualiza ou cria o arquivo com os valores iniciais
-    
-    manipular_configuracao('atualizar', {'contador': contador, 'N': N, 'U': U})
+        U = gf.DEFAULT_U
+
+    contador = config['contador'] if config and 'contador' in config else gf.DEFAULT_CONTADOR
+
+    # Atualiza a URL da instância
+    gf.U = U
+
+    gf.manipular_configuracao('atualizar', {'contador': contador, 'N': N, 'U': U})
 
     try:
         while True:
-            #
             if N > 0:
-                print("Contador :", contador)
+                print("Contador:", contador)
                 W = 30
-                get_mnemonic_seq(int(contador), int(N))
+                get_mnemonic_seq(contador, N)
                 contador += 1
-                manipular_configuracao('atualizar', {'contador': contador, 'N': N})
+                gf.manipular_configuracao('atualizar', {'contador': contador, 'N': N})
             else:
                 W = 5
                 get_mnemonic_rand()
+
             if contador > 2047:
-                manipular_configuracao('atualizar', {'contador': 0, 'N': N})
+                gf.manipular_configuracao('atualizar', {'contador': 0, 'N': N})
                 break
-            #print("sleep for ", W, " seconds - because no one is made of iron")
-            #time.sleep(W)  # Sleep for 30 seconds
+
+            # time.sleep(W)  # Descomente se quiser pausas entre execuções
     except KeyboardInterrupt:
         print("\n🛑 Loop stopped by user.")
-#
+
 if __name__ == "__main__":
-    # Call the main function  and start aplication
     main()
-
-
-
-
